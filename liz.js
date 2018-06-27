e891469e369a66534fc7df379e0bd414fb301421
James Wu <jwu@efunda.com>
2018-06-26 22:56:41 -0400

    additional stylesheets

--- liz.js
+++ liz.js
@@ -1,1 +1,15 @@
-alert('disjoint');
+function insertStyles(type) {
+  var link = document.createElement('link');
+  link.href = chrome.runtime.getURL(type + '.css');
+  link.type = 'text/css';
+  link.rel = 'stylesheet';
+  document.documentElement.insertBefore(link, null);
+}
+
+insertStyles('square-avatars');
+insertStyles('hide-moments');
+insertStyles('liked-tweets');
+insertStyles('small-media');
+insertStyles('show-links');
+
+// also eventually hidden divs from the DOM
